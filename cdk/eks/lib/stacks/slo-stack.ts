0516bab8ba110492af8abe63d437a11db86e7c92
t_hibira <thibira@gmail.com>
2025-08-29 19:01:33 +0900

    Update slo-stack.ts

--- cdk/eks/lib/stacks/slo-stack.ts
+++ cdk/eks/lib/stacks/slo-stack.ts
@@ -1,8 +1,11 @@
 import { Construct } from 'constructs';
-import { aws_applicationsignals as applicationsignals, Stack, StackProps, Tag } from 'aws-cdk-lib';
+import { aws_applicationsignals as applicationsignals, Stack, StackProps, Tag, Duration } from 'aws-cdk-lib';
 import { AwsCustomResource, AwsCustomResourcePolicy, PhysicalResourceId } from 'aws-cdk-lib/custom-resources';
 import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
-import {CfnServiceLevelObjective} from "aws-cdk-lib/aws-applicationsignals";
+import { CfnServiceLevelObjective } from "aws-cdk-lib/aws-applicationsignals";
+import { Topic } from 'aws-cdk-lib/aws-sns';
+import { Alarm, Metric, ComparisonOperator, TreatMissingData } from 'aws-cdk-lib/aws-cloudwatch';
+import { SnsAction } from 'aws-cdk-lib/aws-cloudwatch-actions';
 import ExclusionWindowProperty = CfnServiceLevelObjective.ExclusionWindowProperty;
 
 
@@ -10,167 +13,220 @@ interface SloProps extends StackProps {
     eksClusterName: string,
     sampleAppNamespace: string,
     awsApplicationTag: string,
-  }
+}
 
 export class SloStack extends Stack {
     private readonly serviceName = 'pet-clinic-frontend-java'
     private readonly endpoint = `https://application-signals.${this.region}.api.aws`;
     private readonly eksClusterName: string;
     private readonly sampleAppNamespace: string;
-      
-  constructor(scope: Construct, id: string, props: SloProps) {
-    super(scope, id, props);
+    private readonly snsAlertTopic: Topic;
 
-    const { eksClusterName, sampleAppNamespace, awsApplicationTag } = props;
+    constructor(scope: Construct, id: string, props: SloProps) {
+        super(scope, id, props);
 
-    this.eksClusterName = eksClusterName;
-    this.sampleAppNamespace = sampleAppNamespace
+        const { eksClusterName, sampleAppNamespace, awsApplicationTag } = props;
 
-    const enableTopologyDiscovery = new AwsCustomResource(this, 'ApplicationSignalsStartDiscovery', {
-        onUpdate: {
-            service: 'application-signals',
-            action: 'StartDiscovery',
-            parameters: {
-                Endpoint: this.endpoint,
-            },
-            physicalResourceId: PhysicalResourceId.of('StartDiscoveryOperation'),
-        },
-        policy: AwsCustomResourcePolicy.fromStatements([
-            new PolicyStatement({
-                actions: [
-                    'application-signals:StartDiscovery',
-                    'iam:CreateServiceLinkedRole',
-                ],
-                resources: ['*'],
-            }),
-        ]),
-    });
+        this.eksClusterName = eksClusterName;
+        this.sampleAppNamespace = sampleAppNamespace;
 
-    const getOwner99AvailabilitySlo = new applicationsignals.CfnServiceLevelObjective(this, 'getOwner99AvailabilitySLO', this.getSloProp(
-        "Availability for Searching an Owner",
-        "Availability larger than 99 for Get Owner operation",
-        "GET",
-        "AVAILABILITY",
-        99.0,
-        "GreaterThan",
-        awsApplicationTag,
-    ));
-    const getOwner99LatencySlo = new applicationsignals.CfnServiceLevelObjective(this, 'getOwner99LatencySLO', this.getSloProp(
-        "Latency for Searching an Owner",
-        "Latency P99 less than 200 ms for Get Owner operation",
-        "GET",
-        "LATENCY",
-        200.0,
-        "LessThan",
-        awsApplicationTag,
-        "p99"
-    ));
-    const postOwner99AvailabilitySlo = new applicationsignals.CfnServiceLevelObjective(this, 'postOwner99AvailabilitySLO', this.getSloProp(
-        "Availability for Registering an Owner",
-        "Availability larger than 99 for Post Owner operation",
-        "POST",
-        "AVAILABILITY",
-        99.0,
-        "GreaterThan",
-        awsApplicationTag,
-    ));
-    const postOwner99LatencySlo = new applicationsignals.CfnServiceLevelObjective(this, 'postOwner99LatencySLO', this.getSloProp(
-        "Latency for Registering an Owner",
-        "Latency P99 less than 2000 ms for Post Owner operation",
-        "POST",
-        "LATENCY",
-        2000.0,
-        "LessThan",
-        awsApplicationTag,
-        "p99"
-    ));
-    const billingActivitiesLatencySlo = new applicationsignals.CfnServiceLevelObjective(this, 'billingActivitiesLatencySLO', this.getSloProp(
-        "Latency for Billing Activities",
-        "Latency P99 less than 300 ms for GET Billing Activities operation",
-        "GET",
-        "LATENCY",
-        300.0,
-        "LessThan",
-        awsApplicationTag,
-        "p99",
-        "^billings/$",
-        "billing-service-python"
-    ));
+        // Create SNS topic for SLO alerts
+        this.snsAlertTopic = new Topic(this, 'SloAlertTopic', {
+            topicName: 'slo-alert',
+            displayName: 'SLO Alert Notifications'
+        });
 
-    const exclusionWindows = [{
-         window: {
-             duration: 16,
-             durationUnit: 'HOUR',
-         },
-         recurrenceRule: {
-             expression: 'cron(0 17 ? * *)',
-         }
-    }]
-    const getPaymentAvailabilitySlo = new applicationsignals.CfnServiceLevelObjective(this, 'getPaymentAvailabilitySLO', this.getSloProp(
-        "Availability for Retrieving Payments",
-        "Availability larger than 99 for Get Payment operation",
-        "GET",
-        "AVAILABILITY",
-        99.0,
-        "GreaterThan",
-        awsApplicationTag,
-        undefined,
-        "/owners/{ownerId:int}/pets/{petId:int}/payments/",
-        "payment-service-dotnet",
-        undefined,
-        exclusionWindows
-    ));
-
-    getOwner99AvailabilitySlo.node.addDependency(enableTopologyDiscovery);
-    getOwner99LatencySlo.node.addDependency(enableTopologyDiscovery);
-    postOwner99AvailabilitySlo.node.addDependency(enableTopologyDiscovery);
-    postOwner99LatencySlo.node.addDependency(enableTopologyDiscovery);
-    billingActivitiesLatencySlo.node.addDependency(enableTopologyDiscovery);
-    getPaymentAvailabilitySlo.node.addDependency(enableTopologyDiscovery);
-  }
-  
-  getSloProp(name: string, description: string, requestType: string, metricType: string, metricThreshold: number, comparisonOperator: string, awsApplicationTag: string, statistic?: string, operationPath?: string, serviceName?: string, serviceType?: string, exclusionWindows?: ExclusionWindowProperty[]) {
-    // Default values
-    const path = operationPath || '/api/customer/owners';
-    const service = serviceName || this.serviceName;
-    const type = serviceType || 'Service';
-    const environment = type === 'Lambda' ? 
-      undefined : 
-      { "Environment": `eks:${this.eksClusterName}/${this.sampleAppNamespace}` };
-    
-    const sloProp: applicationsignals.CfnServiceLevelObjectiveProps = {
-        name: name, 
-        description: description,
-        sli: {
-            sliMetric: {
-                keyAttributes: {
-                    ...(environment || {}),
-                    "Name": service,
-                    "Type": type
+        const enableTopologyDiscovery = new AwsCustomResource(this, 'ApplicationSignalsStartDiscovery', {
+            onUpdate: {
+                service: 'application-signals',
+                action: 'StartDiscovery',
+                parameters: {
+                    Endpoint: this.endpoint,
                 },
-                operationName: type === 'Lambda' ? undefined : `${requestType} ${path}`,
-                metricType: metricType,
-                statistic: statistic,
-                periodSeconds: 60
+                physicalResourceId: PhysicalResourceId.of('StartDiscoveryOperation'),
             },
-            metricThreshold: metricThreshold,
-            comparisonOperator: comparisonOperator,
-          },
-          goal: {
-            interval: {
-                rollingInterval: {
-                    duration: 1,
-                    durationUnit: "DAY",
-                }
+            policy: AwsCustomResourcePolicy.fromStatements([
+                new PolicyStatement({
+                    actions: [
+                        'application-signals:StartDiscovery',
+                        'iam:CreateServiceLinkedRole',
+                    ],
+                    resources: ['*'],
+                }),
+            ]),
+        });
+
+        const getOwner99AvailabilitySlo = new applicationsignals.CfnServiceLevelObjective(this, 'getOwner99AvailabilitySLO', this.getSloProp(
+            "Availability for Searching an Owner",
+            "Availability larger than 99 for Get Owner operation",
+            "GET",
+            "AVAILABILITY",
+            99.0,
+            "GreaterThan",
+            awsApplicationTag,
+        ));
+        const getOwner99LatencySlo = new applicationsignals.CfnServiceLevelObjective(this, 'getOwner99LatencySLO', this.getSloProp(
+            "Latency for Searching an Owner",
+            "Latency P99 less than 200 ms for Get Owner operation",
+            "GET",
+            "LATENCY",
+            200.0,
+            "LessThan",
+            awsApplicationTag,
+            "p99"
+        ));
+        const postOwner99AvailabilitySlo = new applicationsignals.CfnServiceLevelObjective(this, 'postOwner99AvailabilitySLO', this.getSloProp(
+            "Availability for Registering an Owner",
+            "Availability larger than 99 for Post Owner operation",
+            "POST",
+            "AVAILABILITY",
+            99.0,
+            "GreaterThan",
+            awsApplicationTag,
+        ));
+        const postOwner99LatencySlo = new applicationsignals.CfnServiceLevelObjective(this, 'postOwner99LatencySLO', this.getSloProp(
+            "Latency for Registering an Owner",
+            "Latency P99 less than 2000 ms for Post Owner operation",
+            "POST",
+            "LATENCY",
+            2000.0,
+            "LessThan",
+            awsApplicationTag,
+            "p99"
+        ));
+        const billingActivitiesLatencySlo = new applicationsignals.CfnServiceLevelObjective(this, 'billingActivitiesLatencySLO', this.getSloProp(
+            "Latency for Billing Activities",
+            "Latency P99 less than 300 ms for GET Billing Activities operation",
+            "GET",
+            "LATENCY",
+            300.0,
+            "LessThan",
+            awsApplicationTag,
+            "p99",
+            "^billings/$",
+            "billing-service-python"
+        ));
+
+        const exclusionWindows = [{
+            window: {
+                duration: 16,
+                durationUnit: 'HOUR',
             },
-            attainmentGoal: 99.9,
-            warningThreshold: 60.0,
-          },
-          tags: [
-            new Tag("awsApplication", awsApplicationTag)
-          ],
-          exclusionWindows: exclusionWindows
+            recurrenceRule: {
+                expression: 'cron(0 17 ? * *)',
+            }
+        }]
+        const getPaymentAvailabilitySlo = new applicationsignals.CfnServiceLevelObjective(this, 'getPaymentAvailabilitySLO', this.getSloProp(
+            "Availability for Retrieving Payments",
+            "Availability larger than 99 for Get Payment operation",
+            "GET",
+            "AVAILABILITY",
+            99.0,
+            "GreaterThan",
+            awsApplicationTag,
+            undefined,
+            "/owners/{ownerId:int}/pets/{petId:int}/payments",
+            "payment-service-dotnet",
+            // undefined,
+            // exclusionWindows
+        ));
+
+        const postOwnerPet99AvailabilitySlo = new applicationsignals.CfnServiceLevelObjective(this, 'postOwnerPet99AvailabilitySLO', this.getSloProp(
+            "Availability for Owners Pets",
+            "Availability larger than 99 for Post Owners Pet operation",
+            "POST",
+            "AVAILABILITY",
+            99.0,
+            "GreaterThan",
+            awsApplicationTag,
+            undefined,
+            "/api/customer/owners/{ownerId}/pets",
+            "pet-clinic-frontend-java"
+        ));
+
+        getOwner99AvailabilitySlo.node.addDependency(enableTopologyDiscovery);
+        getOwner99LatencySlo.node.addDependency(enableTopologyDiscovery);
+        postOwner99AvailabilitySlo.node.addDependency(enableTopologyDiscovery);
+        postOwner99LatencySlo.node.addDependency(enableTopologyDiscovery);
+        billingActivitiesLatencySlo.node.addDependency(enableTopologyDiscovery);
+        getPaymentAvailabilitySlo.node.addDependency(enableTopologyDiscovery);
+        postOwnerPet99AvailabilitySlo.node.addDependency(enableTopologyDiscovery);
+
+        // Create alarms for SLO violations
+        this.createSloAlarm(getOwner99AvailabilitySlo, 'GetOwnerAvailability');
+        this.createSloAlarm(getOwner99LatencySlo, 'GetOwnerLatency');
+        this.createSloAlarm(postOwner99AvailabilitySlo, 'PostOwnerAvailability');
+        this.createSloAlarm(postOwner99LatencySlo, 'PostOwnerLatency');
+        this.createSloAlarm(billingActivitiesLatencySlo, 'BillingActivitiesLatency');
+        this.createSloAlarm(getPaymentAvailabilitySlo, 'GetPaymentAvailability');
+        this.createSloAlarm(postOwnerPet99AvailabilitySlo, 'PostOwnerPetAvailability');
     }
-    return sloProp
-  }
+
+    getSloProp(name: string, description: string, requestType: string, metricType: string, metricThreshold: number, comparisonOperator: string, awsApplicationTag: string, statistic?: string, operationPath?: string, serviceName?: string, serviceType?: string, exclusionWindows?: ExclusionWindowProperty[]) {
+        // Default values
+        const path = operationPath || '/api/customer/owners';
+        const service = serviceName || this.serviceName;
+        const type = serviceType || 'Service';
+        const environment = type === 'Lambda' ?
+            undefined :
+            { "Environment": `eks:${this.eksClusterName}/${this.sampleAppNamespace}` };
+
+        const sloProp: applicationsignals.CfnServiceLevelObjectiveProps = {
+            name: name,
+            description: description,
+            sli: {
+                sliMetric: {
+                    keyAttributes: {
+                        ...(environment || {}),
+                        "Name": service,
+                        "Type": type
+                    },
+                    operationName: type === 'Lambda' ? undefined : `${requestType} ${path}`,
+                    metricType: metricType,
+                    statistic: statistic,
+                    periodSeconds: 60
+                },
+                metricThreshold: metricThreshold,
+                comparisonOperator: comparisonOperator,
+            },
+            goal: {
+                interval: {
+                    rollingInterval: {
+                        duration: 1,
+                        durationUnit: "DAY",
+                    }
+                },
+                attainmentGoal: 99.9,
+                warningThreshold: 60.0,
+            },
+            tags: [
+                new Tag("awsApplication", awsApplicationTag)
+            ],
+            exclusionWindows: exclusionWindows
+        }
+        return sloProp
+    }
+
+    private createSloAlarm(slo: applicationsignals.CfnServiceLevelObjective, alarmName: string) {
+        const alarm = new Alarm(this, `${alarmName}SloAlarm`, {
+            alarmName: `SLO-Violation-${alarmName}`,
+            alarmDescription: `SLO violation alarm for ${slo.name}`,
+            metric: new Metric({
+                namespace: 'AWS/ApplicationSignals',
+                metricName: 'BreachedCount',
+                dimensionsMap: {
+                    'SloName': slo.name!
+                },
+                statistic: 'Sum',
+                period: Duration.minutes(5)
+            }),
+            threshold: 1, // Alert on any breach
+            comparisonOperator: ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+            evaluationPeriods: 2,
+            treatMissingData: TreatMissingData.NOT_BREACHING
+        });
+
+        alarm.addAlarmAction(new SnsAction(this.snsAlertTopic));
+        alarm.node.addDependency(slo);
+    }
 }
